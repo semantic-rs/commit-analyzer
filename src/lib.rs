5f06b1d6553f666b84d4d11b5976ded7bdce4b90
Jan-Erik Rediger <janerik@fnordig.de>
2015-12-28 15:04:54 +0100

    Parse a commit message correctly

--- src/lib.rs
+++ src/lib.rs
@@ -3,82 +3,79 @@ extern crate clog;
 use clog::Clog;
 use clog::error::Error;
 
-#[derive(PartialEq,Eq,Debug)]
+#[derive(PartialEq,Eq,Debug,PartialOrd,Ord)]
 pub enum CommitType {
     Unknown,
-    Major,
-    Minor,
     Patch,
+    Minor,
+    Major,
 }
 
 use CommitType::*;
 
-pub fn analyze(commits: &[&str]) -> Result<CommitType,Error> {
-    let mut commit_type = Unknown;
+pub fn analyze_single(commit: &str) -> Result<CommitType, Error> {
+    let clog = Clog::new().unwrap();
+    let commit = clog.parse_raw_commit(commit);
 
-    let clog = try!(Clog::new());
+    if commit.breaks.len() > 0 {
+        return Ok(Major);
+    }
 
-    commits
-        .iter()
-        .map(|message| clog.parse_raw_commit(message))
-        .all(|commit| {
-            if commit.breaks.len() > 0 {
-                commit_type = Major;
-                return false;
-            }
-
-            if commit.commit_type == "feat" {
-                commit_type = Minor;
-            }
-
-            if commit_type == Unknown && commit.commit_type == "fix" {
-                commit_type = Patch;
-            }
-
-            return true;
-        });
+    let commit_type = match &commit.commit_type[..] {
+        "Features" => Minor,
+        "Bug Fixes" => Patch,
+        _ => Unknown,
+    };
 
     Ok(commit_type)
 }
 
+pub fn analyze(commits: &[&str]) -> Result<CommitType, Error> {
+    let commit_type = commits.into_iter()
+                             .map(|commit| analyze_single(commit).unwrap())
+                             .map(|commit| {
+                                 println!("commit: {:?}", commit);
+                                 commit
+                             })
+                             .max();
+
+    Ok(commit_type.unwrap())
+}
+
 #[test]
 fn unknown_type() {
-    let commits = vec!["This commit message has no type"];
+    let commits = vec!["0\nThis commit message has no type"];
     assert_eq!(Unknown, analyze(&commits).unwrap());
 }
 
 #[test]
 fn patch_commit() {
-    let commits = vec!["fix: This commit fixes a bug"];
-    assert_eq!(Unknown, analyze(&commits).unwrap());
+    let commits = vec!["0\nfix: This commit fixes a bug"];
+    assert_eq!(Patch, analyze(&commits).unwrap());
+    assert_eq!(Patch, analyze_single(commits[0]).unwrap());
 }
 
 #[test]
 fn minor_commit() {
-    let commits = vec!["feat: This commit introduces a new feature"];
-    assert_eq!(Unknown, analyze(&commits).unwrap());
+    let commits = vec!["0\nfeat: This commit introduces a new feature"];
+    assert_eq!(Minor, analyze(&commits).unwrap());
 }
 
 #[test]
 fn major_commit() {
-    let commits = vec!["feat: This commits breaks something\nBREAKING CHANGE: breaks things"];
-    assert_eq!(Unknown, analyze(&commits).unwrap());
+    let commits = vec!["0\nfeat: This commits breaks something\nBREAKING CHANGE: breaks things"];
+    assert_eq!(Major, analyze(&commits).unwrap());
 }
 
 #[test]
 fn major_commit_multiple() {
-    let commits = vec![
-        "feat: This commits breaks something\r\n\r\nBREAKING CHANGE: breaks things",
-        "fix: Simple fix"
-    ];
-    assert_eq!(Unknown, analyze(&commits).unwrap());
+    let commits = vec!["0\nfeat: This commits breaks something\n\nBREAKING CHANGE: breaks things",
+                       "0\nfix: Simple fix"];
+    assert_eq!(Major, analyze(&commits).unwrap());
 }
 
 #[test]
 fn minor_commit_multiple() {
-    let commits = vec![
-        "feat: This commits introduces a new feature",
-        "fix: Simple fix"
-    ];
-    assert_eq!(Unknown, analyze(&commits).unwrap());
+    let commits = vec!["0\nfeat: This commits introduces a new feature", "fix: Simple fix"];
+    assert_eq!(Minor, analyze(&commits).unwrap());
 }
